1e9433c266e19d30d9cc056f3ad3700f2f1b7103
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-08-14 20:42:45 -0400

    Feat: routing via Link and BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
-// import Search from './Search';
+import Search from './Search';
 import BookShelf from './BookShelf'
 
 
 class App extends Component {
   state = {
-    books: []
+    query: []
   }
   
   render() {
     return (
       <div className="App">
-        <BookShelf />
+        <Route exact path='/' render={() => (
+          <div>
+            <header className='list-books-title'>
+              <h1>My Reads</h1>
+            </header>
+            <BookShelf shelfTitle='Currently Reading' />
+            <BookShelf shelfTitle='Want to Read' />
+            <BookShelf shelfTitle='Already Read' />
+          </div>
+        )}/>
+        <Route path='/search' render={() => (
+          <Search/>
+        )}/>
       </div>
     );
   }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,28 +3,16 @@ import Book from './Book';
 
 
 class BookShelf extends Component {
+  
   render() {
     return (
       <div className='list-books'>
-        <header className='list-books-title'>
-          <h1>My Reads</h1>
-        </header>
         <div className='bookshelf'>
           <div className='bookshelf-title'>
-            <h2>Currently Reading</h2>
-            <div className='bookshelf-books'><Book/></div>
-          </div>
-        </div>
-        <div className='bookshelf'>
-          <div className='bookshelf-title'>
-            <h2>Want to Read</h2>
-            <div className='bookshelf-books'><Book/></div>
-          </div>
-        </div>
-        <div className='bookshelf'>
-          <div className='bookshelf-title'>
-            <h2>Already Read</h2>
-            <div className='bookshelf-books'><Book/></div>
+            <h2>{this.props.shelfTitle}</h2>
+            <div className='bookshelf-books'>
+              <Book shelf={this.props.shelf}/>
+            </div>
           </div>
         </div>
       </div>
