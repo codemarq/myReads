ba3809329229089acca0cac3c27aa39f5ea8ff22
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-08-24 15:44:09 -0400

    FEAT: setup shelf changer menu uncontrolled component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,23 +1,43 @@
-import React, { Component } from 'react';
-import Book from './Book';
+import React from 'react';
+// eslint-disable-next-line
+// import Book from './Book';
+import ShelfChanger from './ShelfChanger';
 
 
-class BookShelf extends Component {
+function BookShelf (props) {
   
-  render() {
-    return (
-      <div className='list-books'>
-        <div className='bookshelf'>
-          <div className='bookshelf-title'>
-            <h2>{this.props.shelfTitle}</h2>
-            <div className='bookshelf-books'>
-              <Book shelf={this.props.shelf}/>
-            </div>
+  return (
+    <div className='list-books'>
+      <div className='bookshelf'>
+        <div className='bookshelf-title'>
+          <h2>{props.shelfTitle}</h2>
+          <div className='bookshelf-books'>
+            <ol className='books-grid'>
+              {props.books.map(book => (
+                <li key={book.id}>
+                  <div className='book'>
+                    <div className='book-top'>
+                      <div className="book-cover" 
+                        style={{ 
+                          width: 128, 
+                          height: 193, 
+                          backgroundImage: 'url(' + book.imageLinks.thumbnail + ')' 
+                        }}>
+                      </div>
+                      <ShelfChanger selected={book.shelf}/>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                  </div>
+                </li>
+              ))}
+            </ol>
           </div>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  )
+  
+};
 
 export default BookShelf;
