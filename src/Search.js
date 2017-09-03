a21bdb4aa58ea9ff7b68af406f187b8ec09d64c7
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-09-03 10:02:52 -0400

    CHORE: refactor-new component BookCase
    TODO:
    Broke out main page to a `BookCase` component.  Now `App.js` only has to worry about rendering routes and handling state of the books object.
    Still need to get the search page functional and the select option change handler.  then complete.

--- src/Search.js
+++ src/Search.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-
+import BookShelf from './BookShelf';
 
 class Search extends Component {
-	handleSubmit = (e) => {
-		e.preventDefault()
-
-	}
+	
 
 	render () {
 		return (
 			<div className='search-books'>
 				<div className='search-books-bar'>
-					<Link className='close-search' to='/'>Close</Link>
+					<Link 
+						to='/'
+						className='close-search' 
+					>Close</Link>
 					<form className='search-books-input-wrapper'>
 							<input type='text' name='search' placeholder='Search by title or author'/>
 					</form>
 				</div>
 				<div className='search-books-results'>
-					<ol className='books-grid'></ol>
+					<BookShelf shelfTitle='' books={this.props.books}/>
 				</div>
 			</div>
 		)
